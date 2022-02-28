13fc74b8fa553978d77ebcf3165f4f2849fd6b2e
Valentina Maraio <valentina.maraio89@gmail.com>
2022-02-28 20:55:15 +0100

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -1,40 +1,19 @@
-/*fetch('https://rickandmortyapi.com/api/character/')
-.then(res => res.json())
-.then(characters => showCharacters(characters.results));
-
-showCharacters = characters => { 
-    const charactersDiv = document.querySelector('#ram'); 
-    characters.forEach(character => {
-        const characterElement = document.createElement('p');
-        characterElement.innerText = `Character Name: ${character.name}`;
-        charactersDiv.append(characterElement);
-
-        //buttons
-        const characterButton = document.createElement('button');
-        characterButton.innerHTML = 'Click';
-        charactersDiv.append(characterButton);
-
-        characterButton.addEventListener('click', function() {
-            fetch('https://rickandmortyapi.com/api/location')
-            .then(function (result) {
-                console.log(result.location);
-            })
-            .catch(function (e) {
-                console.log(e);
-            });
-        });
-    });
-}*/
-
-fetch('https://www.fruityvice.com/api/fruit/all')
-.then(res => res.json())
-.then(fruits => showFruits(fruits.name));
-
-showFruit = fruits => {
-    const fruitsDiv = document.querySelector('#fruit');
-    fruits.forEach(fruit => {
-        const fruitElement = document.createElement('li');
-        fruitElement.innerHTML = `Fruit Names: ${fruit.name}`;
-        fruitsDiv.append(fruitElement);
+    axios({
+        method: 'GET',
+        url: 'https://www.fruityvice.com/api/fruit/all'
+    }).then(res => {
+        const fruitName = document.getElementById('name')
+        const fruitFamily = document.getElementById('family')
+        const fragment = document.createDocumentFragment()
+        for(const fruit of res.data) {
+            const fruitNameH2 = document.createElement('h2')
+            fruitNameH2.textContent = `${fruit.name}`
+            const fruitFamilyH3 = document.createElement('h3')
+            fruitFamilyH3.textContent = `${fruit.family}`
+            fragment.appendChild(fruitNameH2)
+            fragment.appendChild(fruitFamilyH3)
+        }
+        fruitName.appendChild(fragment)
+        fruitFamily.appendChild(fragment)
     })
-}
+    .catch(error => console.log(error))
