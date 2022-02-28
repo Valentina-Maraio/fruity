9b20bb6675de8db3168a56a502a8d42ec0ddf235
Valentina Maraio <valentina.maraio89@gmail.com>
2022-02-28 21:07:39 +0100

    works

--- JS/main.js
+++ JS/main.js
@@ -1,19 +1,19 @@
-    axios({
-        method: 'GET',
-        url: 'https://www.fruityvice.com/api/fruit/all'
-    }).then(res => {
-        const fruitName = document.getElementById('name')
-        const fruitFamily = document.getElementById('family')
-        const fragment = document.createDocumentFragment()
-        for(const fruit of res.data) {
-            const fruitNameH2 = document.createElement('h2')
-            fruitNameH2.textContent = `${fruit.name}`
-            const fruitFamilyH3 = document.createElement('h3')
-            fruitFamilyH3.textContent = `${fruit.family}`
-            fragment.appendChild(fruitNameH2)
-            fragment.appendChild(fruitFamilyH3)
-        }
-        fruitName.appendChild(fragment)
-        fruitFamily.appendChild(fragment)
-    })
-    .catch(error => console.log(error))
+axios({
+    method: 'GET',
+    url: 'https://www.fruityvice.com/api/fruit/all'
+}).then(res => {
+    const fruitName = document.getElementById('name')
+    const fruitFamily = document.getElementById('family')
+    const fragment = document.createDocumentFragment()
+    for(const fruit of res.data) {
+        const fruitNameH2 = document.createElement('h2')
+        fruitNameH2.textContent = `${fruit.name}`
+        const fruitFamilyH3 = document.createElement('h3')
+        fruitFamilyH3.textContent = `${fruit.family}`
+        fragment.appendChild(fruitNameH2)
+        fragment.appendChild(fruitFamilyH3)
+    }
+    fruitName.appendChild(fragment)
+    fruitFamily.appendChild(fragment)
+})
+.catch(error => console.log(error))
